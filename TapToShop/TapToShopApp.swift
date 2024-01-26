171ac1eee10433614f9322e9805958c00e0dca43
mayurnilwant <mayurnilwant@gmail.com>
2024-01-25 23:54:14 -0500

    Initial Commit

---
+++ TapToShop/TapToShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapToShopApp.swift
+//  TapToShop
+//
+//  Created by Mayur Nilwant on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TapToShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
