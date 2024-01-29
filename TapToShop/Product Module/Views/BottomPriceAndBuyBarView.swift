ba00678f6fae9344a5565a572e3ec7cfff13e014
mayurnilwant <mayurnilwant@gmail.com>
2024-01-28 23:39:19 -0500

    1) Designed ItemDetailScreeen. 2) Worked on layout.

---
+++ TapToShop/Product Module/Views/BottomPriceAndBuyBarView.swift
@@ -0,0 +1,18 @@
+//
+//  BottomPriceAndBuyBarView.swift
+//  TapToShop
+//
+//  Created by Mayur Nilwant on 28/01/2024.
+//
+
+import SwiftUI
+
+struct BottomPriceAndBuyBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomPriceAndBuyBarView()
+}
---
+++ TapToShop/Product Module/Views/ItemDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetailView.swift
+//  TapToShop
+//
+//  Created by Mayur Nilwant on 28/01/2024.
+//
+
+import SwiftUI
+
+struct ItemDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemDetailView()
+}
